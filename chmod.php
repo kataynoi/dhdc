8ffac4805fba78a971abcab8ae115e65e95db09a
UTEHN <UTEHN@UTEHN-NB>
2015-01-31 16:32:12 +0700

    test on UBUNTU14.04-SERVER

---
+++ chmod.php
@@ -0,0 +1,6 @@
+<?php
+chmod('/var/www/html/dhdc/frontend/web/assets/', 0777);
+chmod('/var/www/html/dhdc/backend/web/assets/', 0777);
+chmod('/var/www/html/dhdc/frontend/web/fortythree/', 0777);
+chmod('/var/www/html/dhdc/frontend/web/fortythreebackup/', 0777);
+
