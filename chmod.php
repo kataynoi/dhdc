b25629fd01cee9aaeeebb3d9982e708891ef9685
UTEHN <UTEHN@UTEHN-NB>
2015-01-31 22:58:44 +0700

    test on UBUNTU14.04-SERVER

--- chmod.php
+++ chmod.php
@@ -1,9 +1,9 @@
 <?php
 
-mkdir('/var/www/html/dhdc/frontend/web/assets/', 0777);
-mkdir('/var/www/html/dhdc/backend/web/assets/', 0777);
-mkdir('/var/www/html/dhdc/frontend/web/fortythree/', 0777);
-mkdir('/var/www/html/dhdc/frontend/web/fortythreebackup/', 0777);
+@mkdir('/var/www/html/dhdc/frontend/web/assets/', 0777);
+@mkdir('/var/www/html/dhdc/backend/web/assets/', 0777);
+@mkdir('/var/www/html/dhdc/frontend/web/fortythree/', 0777);
+@mkdir('/var/www/html/dhdc/frontend/web/fortythreebackup/', 0777);
 
 chmod('/var/www/html/dhdc/frontend/web/assets/', 0777);
 chmod('/var/www/html/dhdc/backend/web/assets/', 0777);
