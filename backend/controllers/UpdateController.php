fa1329f2ff21d3195a4e834bc7a9643091e56b9b
UTEHN <UTEHN@UTEHN-NB>
2015-02-01 19:48:04 +0700

    test on UBUNTU14.04-SERVER

--- backend/controllers/UpdateController.php
+++ backend/controllers/UpdateController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace backend\controllers;
+
 use linslin\yii2\curl;
 
 /**
@@ -10,29 +11,15 @@ use linslin\yii2\curl;
  */
 class UpdateController extends \yii\web\Controller {
 
-    
-    public function actionDatabase() {       
-        
-        //Init curl
-        $curl = new curl\Curl(); 
-        
-        $res = $curl->get('http://utehn.plkhealth.go.th/update/db_list.txt');
-        //print_r($res);
-        $file = explode(',', $res);
-        foreach ($file as $f) {
-            echo $f;echo "<br>";
-            $sql = $curl->get("http://utehn.plkhealth.go.th/update/database/$f");
-            \yii::$app->db->createCommand($sql)->execute();
-        }
-        return;
-        
-       
-        
-       
-        
+    public function actionDatabase() {
+
+        $curl = new curl\Curl();
+        $sql = $curl->get("http://utehn.plkhealth.go.th/update/database/update.sql");
+        \yii::$app->db->createCommand($sql)->execute();
     }
-    public function actionProgram(){
-        
+
+    public function actionProgram() {
+
         $source = "http://utehn.plkhealth.go.th/demo/files/f1.txt";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $source);
@@ -46,7 +33,6 @@ class UpdateController extends \yii\web\Controller {
         $file = fopen($destination, "w+");
         fputs($file, $data);
         fclose($file);
-        
     }
 
 }
