a35b71dff2ddad240aa198ca47d74bb373231ebf
UTEHN <UTEHN@192.168.199.30>
2015-02-02 11:27:18 +0700

    delete broker

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,3 +1,13 @@
+<style>
+    .btn-xlarge {
+        padding: 18px 28px;
+        font-size: 22px; //change this to your desired size
+        line-height: normal;
+        -webkit-border-radius: 8px;
+        -moz-border-radius: 8px;
+        border-radius: 8px;
+    }
+</style>
 <?php
 /* @var $this yii\web\View */
 
@@ -10,34 +20,34 @@ $this->title = 'DHDC Backend';
         $version = \backend\models\SysVersion::find()->one();
         ?>
         <h1>ระบบ District HDC BACK-END</h1>
-        <p>frontend :  <?=$version->frontend;?></p>
-        <p>backend : <?=$version->backend;?></p>
-        <p>database : <?=$version->database;?></p>
+        <p>frontend :  <?= $version->frontend; ?></p>
+        <p>backend : <?= $version->backend; ?></p>
+        <p>database : <?= $version->database; ?></p>
         <p><a href="" class="btn btn-danger">Check</a></p>
     </div>
-    <div class="container well">
+    <div class="well">
         <div class="row">
             <div class="col-sm-4">
                 <?php
                 $route = \Yii::$app->urlManager->createUrl('sysconfigmain/index');
                 ?>
-                <a class="btn btn-success" id="btn_1" href="<?= $route ?>"> 
-                    ตั้งค่าอำเภอ
+                <a class="btn btn-success btn-xlarge" id="btn_1" href="<?= $route ?>"> 
+                    <i class="glyphicon glyphicon-cog"></i>  ตั้งค่าอำเภอ   
                 </a>
 
             </div>
             <div class="col-sm-4">
 
-                <a class="btn btn-primary" href="<?= Yii::$app->urlManager->createUrl('user/index') ?>"> 
-                    จัดการ USER
+                <a class="btn btn-primary btn-xlarge" href="<?= Yii::$app->urlManager->createUrl('user/index') ?>"> 
+                    <i class="glyphicon glyphicon-user"></i> จัดการ USER   
                 </a>
 
 
             </div>
             <div class="col-sm-4">
 
-                <button class="btn btn-danger" id="btn_process"> 
-                    ประมวลผลรายงาน
+                <button class="btn btn-danger btn-xlarge" id="btn_process"> 
+                    <i class="glyphicon glyphicon-refresh"></i> ประมวลผลรายงาน
                 </button>
                 <div id="res" style="display: none">
                     <img src="images/busy.gif">
@@ -54,15 +64,19 @@ $this->title = 'DHDC Backend';
                 $route_off = Yii::$app->urlManager->createUrl('onoff/off');
                 if ($onoff->status === 'on'):
                     ?>
-                    <a class="btn btn-danger" href="<?= $route_off ?>">Close Upload</a>
+                    <a class="btn btn-danger btn-xlarge" href="<?= $route_off ?>">
+                        <i class="glyphicon glyphicon-folder-open"></i> Close Upload
+                    </a>
                 <?php else: ?>
-                    <a class="btn btn-success" href="<?= $route_on ?>">Open Upload</a>
+                    <a class="btn btn-success btn-xlarge" href="<?= $route_on ?>">
+                        <i class="glyphicon glyphicon-folder-open"></i> Open Upload
+                    </a>
                 <?php endif; ?>
             </div>
             <div class="col-sm-4">
 
-                <a class="btn btn-warning" href="<?= Yii::$app->urlManager->createUrl('execute/index') ?>"> 
-                    Show Processlist
+                <a class="btn btn-warning btn-xlarge" href="<?= Yii::$app->urlManager->createUrl('execute/index') ?>"> 
+                    <i class="glyphicon glyphicon-play"></i> MySQL Load
                 </a>
 
 
@@ -71,8 +85,8 @@ $this->title = 'DHDC Backend';
                 <?php
                 $route = \Yii::$app->urlManager->createUrl('syssettime/index');
                 ?>
-                <a class="btn btn-success" id="btn_set_process" href="<?= $route ?>"> 
-                    ตั้งเวลาประมวลผลรายงาน
+                <a class="btn btn-success btn-xlarge" id="btn_set_process" href="<?= $route ?>"> 
+                    <i class="glyphicon glyphicon-time"></i> ตั้งเวลาประมวลผล
                 </a>
             </div>
         </div>
