deb0985a450dd0a7c30fcfa915f679b45b03f836
UTEHN <UTEHN@UTEHN-NB>
2015-02-07 10:51:38 +0700

    tehnn

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -32,7 +32,7 @@ $this->title = 'DHDC Backend';
                 <button class="btn btn-danger" id="btn_chk_ver">
                     <i class="glyphicon glyphicon-check"></i> Version
                 </button>
-                <a class="btn btn-primary" href="http://203.157.118.117/dhdc_zip/" target="_blank">
+                <a class="btn btn-primary" href="ftp://203.157.118.117/zip/" target="_blank">
                     <i class="glyphicon glyphicon-arrow-up"></i> download
                 </a>
             </div>
