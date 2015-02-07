f59006298ed01e84df38a55caf8b82ee2d21ce30
UTEHN <UTEHN@UTEHN-NB>
2015-02-07 18:45:16 +0700

    tehnn

--- frontend/views/syscountall/index2.php
+++ frontend/views/syscountall/index2.php
@@ -8,65 +8,62 @@ use yii\grid\GridView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'รายแฟ้มรายเดือน';
-$this->params['breadcrumbs'][]= ['label' => 'ผลรวมข้อมูลรายแฟ้ม','url' => ['syscountall/index']];
+$this->params['breadcrumbs'][] = ['label' => 'ผลรวมข้อมูลรายแฟ้ม', 'url' => ['syscountall/index']];
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
 <div class="sys-count-all-index">
     <div style="overflow: auto;">
-    <?=
-    kartik\grid\GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'condensed' => true,
-        'pjax' => true,
-        'pjaxSettings' => [
-            //'neverTimeout' => true,
-            'options' => [
-                'enablePushState' => false,
+        <?=
+        kartik\grid\GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'condensed' => true,
+            'pjax' => true,
+           // 'showPageSummary' => true,
+            'pjaxSettings' => [
+                //'neverTimeout' => true,
+                'options' => [
+                    'enablePushState' => false,
+                ],
             ],
-        ],
-        'responsive' => TRUE,
-        
-        'panel' => [
-            'before' => '',
-            
-        ],
-        'floatHeader' => true,
-        
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'hospcode',
-            [
-                'attribute' => 'month',
-                
+            'responsive' => TRUE,
+            'panel' => [
+                'before' => '',
+                'after'=>''
             ],
-            'person',
-            'death',
-            'service',
-            'accident',
-            'diagnosis_opd',
-            'procedure_opd',
-            'ncdscreen',
-            'chronicfu',
-            'labfu',
-            'chronic',
-            
-            'fp',
-            'epi',
-            'nutrition',
-            'prenatal',
-            'anc',
-            'labor',
-            'postnatal',
-            'newborn',
-            'newborncare',
-            'dental',
-            'admission',
-            'diagnosis_ipd',
-            'procedure_ipd',
-        ],
-    ]);
-    ?>
+            'floatHeader' => true,
+            'columns' => [
+                ['class' => 'kartik\grid\SerialColumn'],
+                'hospcode',
+                [
+                    'attribute' => 'month',
+                ],
+               
+                'person',
+                'death',
+                'service',
+                'accident',
+                'diagnosis_opd',
+                'procedure_opd',
+                'ncdscreen',
+                'chronicfu',
+                'labfu',
+                'chronic',
+                'fp',
+                'epi',
+                'nutrition',
+                'prenatal',
+                'anc',
+                'labor',
+                'postnatal',
+                'newborn',
+                'newborncare',
+                'dental',
+                'admission',
+                'diagnosis_ipd',
+                'procedure_ipd',
+            ],
+        ]);
+        ?>
     </div>
 </div>
