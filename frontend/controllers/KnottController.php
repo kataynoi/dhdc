290bee622a04ba2d074411bfc47177ba3bbaaef9
Administrator <Administrator@192.168.187.102>
2015-02-09 21:32:24 +0700

    เพิ่ม page รายงานอัตราส่วนวินิจฉัยแพทย์แผนไทย

--- frontend/controllers/KnottController.php
+++ frontend/controllers/KnottController.php
@@ -67,6 +67,35 @@ class KnottController extends \yii\web\Controller {
                     'sql' => $sql
         ]);
     }
+    
+                public function actionPanthai_4() {
+
+        $sql = "SELECT d.HOSPCODE,sm.`month`,count(d.SEQ) as total,
+sum(if(d.DIAGCODE like 'u%',1,0)) as panthai_diag,
+round(((sum(if(d.DIAGCODE like 'u%',1,0)))/(count(d.SEQ)))*100,2) as percent
+from diagnosis_opd d,sys_month sm 
+where concat(year(d.DATE_SERV),month(d.DATE_SERV))=sm.`month`
+GROUP BY d.HOSPCODE,concat(year(DATE_SERV),month(DATE_SERV)) 
+ORDER BY d.HOSPCODE,concat(year(DATE_SERV),month(DATE_SERV))";
+        
+        
+        try {
+            $rawData = \Yii::$app->db->createCommand($sql)->queryAll();
+        } catch (\yii\db\Exception $e) {
+            throw new \yii\web\ConflictHttpException('sql error');
+        }
+
+        $dataProvider = new \yii\data\ArrayDataProvider([
+            //'key' => 'hoscode',
+            'allModels' => $rawData,
+            'pagination' => FALSE,
+        ]);
+
+        return $this->render('panthai_4', [
+                    'dataProvider' => $dataProvider,
+                    'sql' => $sql
+        ]);
+    }
 
 }
 
