9ce373dc995c37d4020de9ccbba31af89269e0e9
Administrator <Administrator@192.168.187.102>
2015-02-09 21:53:15 +0700

    แก้ไขรายงานอัตราส่วนการวินิจฉัยด้วยรหัสแพทย์แผนไทย

--- frontend/controllers/KnottController.php
+++ frontend/controllers/KnottController.php
@@ -70,11 +70,11 @@ class KnottController extends \yii\web\Controller {
     
                 public function actionPanthai_4() {
 
-        $sql = "SELECT d.HOSPCODE,sm.`month`,count(d.SEQ) as total,
+        $sql = "SELECT sm.`month`,d.HOSPCODE,amp.hosname,count(d.SEQ) as total,
 sum(if(d.DIAGCODE like 'u%',1,0)) as panthai_diag,
 round(((sum(if(d.DIAGCODE like 'u%',1,0)))/(count(d.SEQ)))*100,2) as percent
-from diagnosis_opd d,sys_month sm 
-where concat(year(d.DATE_SERV),month(d.DATE_SERV))=sm.`month`
+from diagnosis_opd d,sys_month sm,chospital_amp amp 
+where concat(year(d.DATE_SERV),month(d.DATE_SERV))=sm.`month` and d.HOSPCODE=amp.hoscode
 GROUP BY d.HOSPCODE,concat(year(DATE_SERV),month(DATE_SERV)) 
 ORDER BY d.HOSPCODE,concat(year(DATE_SERV),month(DATE_SERV))";
         
